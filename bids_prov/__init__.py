628bdef3e84a682a383258799acbcf1a87b52caf
Rémi Adon <remi.adon@gmail.com>
2021-03-16 12:18:34 +0100

    fsl parser config v0

---
+++ bids_prov/__init__.py
@@ -0,0 +1,28 @@
+import os
+import json
+from os.path import expanduser
+
+
+def get_or_load(fn):
+    """
+    fn should return a json serializable object
+
+    results will be stored in the home directory ('~')
+    """
+
+    def wrapper(*args, **kwargs):
+        filename = os.path.join(expanduser("~"), fn.__name__)
+        filename += "_".join(args)
+        filename += ".json"
+        if os.path.exists(filename):
+            print(f"loading dumped results from {filename}")
+            with open(filename, "r") as fd:
+                d = json.load(fd)
+                return d
+        else:
+            d = fn(*args, **kwargs)
+            with open(filename, "w") as fd:
+                json.dump(d, fd)
+            return d
+
+    return wrapper
